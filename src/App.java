49020ee57718ae87be1303695e25327a6e256205
unknown <mock.jablonski@gmail.com>
2016-12-14 19:18:05 +0000

    Dodanie klasy App

---
+++ src/App.java
@@ -0,0 +1,8 @@
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("=================== Schronisko Dla Kotow v1.0 ====================");
+		System.out.println("+++++++++++++++++++ MENU +++++++++++++++++++++++++++++++++++++++++");
+		System.out.println("");
+	}
+}
